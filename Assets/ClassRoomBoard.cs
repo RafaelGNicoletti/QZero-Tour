7e301c5ca4b3b98449809b784485920d1e61c0db
Andre <jomonzito2010@gmail.com>
2019-09-04 13:08:33 -0300

    Iniciada parte lógica do ClassroomFinder

---
+++ Assets/ClassRoomBoard.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassRoomBoard : MonoBehaviour
+{
+    [System.Serializable]
+    public class ClassRoom
+    {
+        public string codSala;
+        [SerializeField]
+        private string bloco;
+        [SerializeField]
+        private string torre;
+        [SerializeField]
+        private string andar;
+        [SerializeField]
+        private string sala;
+
+        public (bool, List<string>) GetVerification(string bloco, string torre, string andar, string sala)
+        {
+            List<string> camposErrados = new List<string>();
+            bool errado = false;
+
+            if (!bloco.Equals(this.bloco))
+            {
+                errado = true;
+                camposErrados.Add("bloco");
+            }
+            if (!torre.Equals(this.torre))
+            {
+                errado = true;
+                camposErrados.Add("torre");
+            }
+            if (!andar.Equals(this.andar))
+            {
+                errado = true;
+                camposErrados.Add("andar");
+            }
+            if (!sala.Equals(this.sala))
+            {
+                errado = true;
+                camposErrados.Add("sala");
+            }
+
+            return (!errado, camposErrados);
+        }
+    }
+
+    public List<ClassRoom> salas;
+    private int currentClass;
+
+
+}
---
+++ Assets/ClassroomAnswer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClassroomAnswer : MonoBehaviour
+{
+    public Text resposta; //Texto da resposta atual
+    public List<string> valoresValidos; //Lista de valores válidos para o espaço, alterá-los no inspetor
+    private string respostaAtual; //Valor para consulta da string que está ativa como resposta
+    private int currentPosition = 0;
+
+    public string GetRespostaAtual()
+    {
+        return respostaAtual;
+    }
+
+    private void Start()
+    {
+        UpdateAnswer();
+    }
+
+    /// <summary>
+    /// Função de mover um valor para cima ou valor para baixo na lista
+    /// </summary>
+    /// <param name="sinal">É um inteiro em que o sinal é avaliado, se for positivo, avança na lista, caso o contrário, retrocede</param>
+    private void MoveOnList(int sinal)
+    {
+        int listMaxIndex = valoresValidos.Count-1;
+        if (sinal > 0)
+        {
+            if (currentPosition + 1 > listMaxIndex)  //Se a lista chegou no máximo e o jogador quer ir para frente, volta para o início
+            {
+                currentPosition = 0;
+            }
+            else
+            {
+                currentPosition++;
+            }
+        }
+        else
+        {
+            if (currentPosition - 1 < 0)  //Se a lista está no valor 0 e o jogador ir para trás, vai para o final da lista
+            {
+                currentPosition = listMaxIndex;
+            }
+            else
+            {
+                currentPosition--;
+            }
+        }
+        UpdateAnswer();
+    }
+
+    private void UpdateAnswer()
+    {
+        respostaAtual = valoresValidos[currentPosition];
+        resposta.text = valoresValidos[currentPosition];
+    }
+
+    public void GoUp()
+    {
+        MoveOnList(1);
+    }
+
+    public void GoDown()
+    {
+        MoveOnList(-1);
+    }
+}
