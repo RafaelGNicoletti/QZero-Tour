01f3b4fa4e81e28422bff0eb5b6b018c758e37e7
Andre <jomonzito2010@gmail.com>
2019-09-11 16:24:12 -0300

    Script que lê o arquivo da lista de salas criado.

---
+++ Assets/ClassRoomGetter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ClassRoomGetter : MonoBehaviour
+{
+    public TextAsset archive;
+
+    public ClassRoomCollection LoadClassRoom()
+    {
+        return JsonUtility.FromJson<ClassRoomCollection>(archive.text);
+    }
+
+    /*Criado para referência futura, qualquer coisa
+    private void SaveClassRoom(ClassRoomCollection classList)
+    {
+        string classListJson = JsonUtility.ToJson(classList);
+        File.WriteAllText(UnityEditor.AssetDatabase.GetAssetPath(archive), classListJson);
+        UnityEditor.EditorUtility.SetDirty(archive);
+    }*/
+
+}
