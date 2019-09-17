5fbbe9defaa799aa0a6dd083489153d00532e948
Rafael Nicoletti <40860359+RafaelGNicoletti@users.noreply.github.com>
2019-09-17 14:59:50 -0300

    Animação Tutorial

---
+++ Assets/Scripts/TutorialController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    private float playerSpeed, tempMov;
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        ClearSpeed();
+        //playerSpeed = player.GetComponent<PlayerMovement>().movementSpeed;
+        //player.GetComponent<PlayerMovement>().movementSpeed = 0;
+    }
+
+    public void ClearSpeed()
+    {
+        playerSpeed = player.GetComponent<PlayerMovement>().movementSpeed;
+        player.GetComponent<PlayerMovement>().movementSpeed = 0;
+    }
+
+    public void RestoreSpeed()
+    {
+        player.GetComponent<PlayerMovement>().movementSpeed = playerSpeed;
+    }
+
+    public void Toggle(GameObject target)
+    {
+        target.SetActive(!target.active);
+    }
+
+    public void SetAnimationTrigger(string trigger)
+    {
+        this.GetComponent<Animator>().SetTrigger(trigger);
+    }
+}
