e8dcd66b2ac04a686e680462ead0c099e2a32ebb
Andre <jomonzito2010@gmail.com>
2019-09-26 17:25:27 -0300

    Adicionado scripts relacionados com o funcionamento do minigame.

---
+++ Assets/EstanteScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstanteScript : MonoBehaviour, InteractiveObject
+{
+    public string livro;
+
+    public void Interact()
+    {
+        
+    }
+}
