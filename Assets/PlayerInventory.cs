5d8f7130bff7b6ce3713f43a8a34926f5cce16db
Andre <jomonzito2010@gmail.com>
2019-10-05 16:31:39 -0300

    Criado script que cuida do inventário do jogador.

--- Assets/PlayerInventory.cs
+++ Assets/PlayerInventory.cs
@@ -4,5 +4,101 @@ using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
-    private string livro;
+    [SerializeField]
+    private List<InventoryItem> inventory = new List<InventoryItem>();
+
+    public void GiveItem(InventoryItem item)
+    {
+        inventory.Add(item);
+    }
+
+    public InventoryItem TakeFirstItem()
+    {
+        InventoryItem item = null;
+        
+        if (InventoryHaveItens())
+        {
+            item = inventory[0];
+            RetireFirstItem();
+        }
+
+        return item;
+    }
+
+    public InventoryItem TakeItem(string itemName)
+    {
+        InventoryItem item = SearchItem(itemName);
+        
+        if (item)
+        {
+            RetireItem(itemName);
+        }
+
+        return item;
+    }
+
+    public void RetireFirstItem()
+    {
+        if (InventoryHaveItens())
+        {
+            inventory.RemoveAt(0);
+        }
+    }
+
+    public void RetireItem(string itemName)
+    {
+        InventoryItem item = SearchItem(itemName);
+        if(item)
+        {
+            inventory.Remove(item);
+        }
+    }
+
+    public List<InventoryItem> GetAllItems()
+    {
+        return inventory;
+    }
+
+    public InventoryItem GetItemByIndex(int i)
+    {
+        if (inventory.Count>i)
+        {
+            return inventory[i];
+        }
+
+        return null;
+    }
+
+    public bool VerifyItemObtained(string itemName)
+    {
+        if (SearchItem(itemName))
+        {
+            return true;
+        }
+        
+        return false;
+
+    }
+
+    private InventoryItem SearchItem(string itemName)
+    {
+        foreach (InventoryItem item in inventory)
+        {
+            if (itemName == item.GetNome())
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    private bool InventoryHaveItens()
+    {
+        if (inventory.Count > 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
