26985c887dea5d4494ca4eb5190e76a83f8b2b2c
Andre <jomonzito2010@gmail.com>
2019-10-05 16:29:44 -0300

    Criados e atualizados scripts que cuidam de instâncias de objetos (Estantes e Livros).

--- Assets/EstanteScript.cs
+++ Assets/EstanteScript.cs
@@ -4,10 +4,41 @@ using UnityEngine;
 
 public class EstanteScript : MonoBehaviour, InteractiveObject
 {
-    public string livro;
+    [SerializeField]
+    private Livro livro;
+    [SerializeField]
+    private PlayerInventory playerInventory;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInventory = collision.GetComponentInParent<PlayerInventory>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInventory = null;
+        }
+    }
 
     public void Interact()
     {
-        
+        TradeItem();
+    }
+
+    public void TradeItem()
+    {
+        Livro livroVerify = null;
+
+        if (playerInventory)
+        {
+            livroVerify = (Livro) playerInventory.TakeFirstItem();
+            playerInventory.GiveItem(livro);
+            livro = livroVerify;
+        }
     }
 }
