937c079f9169ca31f889b78b532c7ef601412e00
Andre <jomonzito2010@gmail.com>
2019-10-05 16:31:17 -0300

    Atualizada scene Library e adicionado um controlador de jogo.

---
+++ Assets/SceneLibraryController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneLibraryController : MonoBehaviour
+{
+    [System.Serializable]
+    public class TextController
+    {
+        [SerializeField]
+        private Text text;
+        private string currentLivro;
+
+        public void ChangeCurrentLivro(string livroName)
+        {
+            currentLivro = livroName;
+            text.text = currentLivro;
+        }
+    }
+
+    [SerializeField]
+    private TextController textController;
+    [SerializeField]
+    private PlayerInventory playerInventory;
+
+    private void Update()
+    {
+        Livro livro = (Livro) playerInventory.GetItemByIndex(0);
+        if (livro)
+        {
+            textController.ChangeCurrentLivro(livro.GetNome());
+        }
+        else
+        {
+            textController.ChangeCurrentLivro("Nenhum");
+        }
+    }
+}
