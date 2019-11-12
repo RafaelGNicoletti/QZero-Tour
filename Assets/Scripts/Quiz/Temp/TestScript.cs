4015ad90e5b7364fff4da259f938a83f9c95c3a3
Rafael Nicoletti <40860359+RafaelGNicoletti@users.noreply.github.com>
2019-11-12 17:37:52 -0300

    Update
    Criado um modo mais símples de adicionar as perguntas ao quiz através de um arquivo

---
+++ Assets/Scripts/Quiz/Temp/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public ClassPergunta temp;
+    public Perguntas teste;
+    public TextAsset file;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConverterFromJson<ClassPergunta> converter = new ConverterFromJson<ClassPergunta>();
+        temp = converter.Convert(file);
+        teste.SetQuizQuestions(temp);
+    }
+}
