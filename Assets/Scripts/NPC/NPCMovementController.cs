b1ed9da2d3aec346a6e7b3e207cc1dfb140f3833
Rafael Nicoletti <40860359+RafaelGNicoletti@users.noreply.github.com>
2019-11-18 17:49:30 -0300

    Animações
    Inicio das animações de idle dos NPCs
    Script para tocar a animação (direção e se está andando)
    Prefab NPC (troca animator)

---
+++ Assets/Scripts/NPC/NPCMovementController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovementController : MonoBehaviour
+{
+    private Dictionary<string, string> idleDirectionDictionary = new Dictionary<string, string>
+    {
+        { "cima", "Idle_N" },
+        { "baixo", "Idle_S" },
+        { "esquerda", "Idle_O" },
+        { "esq", "Idle_O" },
+        { "direita", "Idle_L" },
+        { "dir", "Idle_L" }
+    };
+
+    public bool isWalking;
+    [Tooltip("Direções aceitas: cima, baixo, esquerda (ou esq), direita (ou dir)")]
+    public string idleDirection;
+
+    [SerializeField]
+    private Animator animatorNPC;
+
+    void Start()
+    {        
+        if (isWalking)
+        {
+            // Código para movimentar o NPC se ele estiver andando - FUTURO
+        }
+        else
+        {
+            // Código para selecionar a direção que o NPC vai estar parado olhando
+            if (idleDirectionDictionary.ContainsKey(idleDirection))
+            {
+                animatorNPC.Play(idleDirectionDictionary[idleDirection]);
+            }
+            else
+            {
+                Debug.LogError("Direção não existente no dicionário");
+            }
+        }
+    }
+}
