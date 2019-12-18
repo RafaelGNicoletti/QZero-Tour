86afb5b2a4c8d49d9ae1a815eaf6158ed9fd9e2d
Andre <jomonzito2010@gmail.com>
2019-12-18 03:51:58 -0300

    Scene sendo atualizada para mostrar feedback ao jogador

---
+++ Assets/ClassroomResultManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClassroomResultManager : MonoBehaviour
+{
+    [SerializeField]
+    private string answerRight;
+    [SerializeField]
+    private string answerWrong;
+    [SerializeField]
+    private Text text;
+    private bool buttonChangeScene; //Colocar true caso o jogador acerte e o clique no botão seja para mudar a scene.
+
+    public void ButtonScript()//Botão irá chamar essa função, que dependendo do valor do buttonChangeScene, fecha a aba de resultado ou muda de scene
+    {
+
+    }
+
+    public void ShowResult(bool acertou, List<string> erros) //Recebe os valores a partir do SceneClassroomFinderController e faz alguma coisa
+    {
+
+    }
+
+}
